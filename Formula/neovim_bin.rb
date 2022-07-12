3c2c9401fc13985d1ff80d526ce2271c7121acb7
Lucas Mendelowski <lucas@mendelowski.com>
2022-07-12 20:23:00 +0200

    Upgrade NeoVim

--- Formula/neovim_bin.rb
+++ Formula/neovim_bin.rb
@@ -1,16 +1,16 @@
 class NeovimBin < Formula
   desc "Binary based formula for installing Neovim on MacOS."
   homepage "https://neovim.io"
-  url "https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-macos.tar.gz"
-  version "0.7.0"
-  sha256 "6260a2edb2da35af02b986c8a6506138afcb3f78f81a80734214b2cadf390a42"
+  url "https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-macos.tar.gz"
+  version "0.7.2"
+  sha256 "39cb6b62f965dccebe433083e33974e1af3c4579a14bf818066b3d5250968495"
 
-  conflicts_with "neovim", because: "neovim also ships a nvim binary"
+  conflicts_with "neovim", because: "neovim also ships neovim binary"
 
   def install
     chmod 0755, "bin/nvim"
 
-    libexec.install "bin", "libs", "lib", "share"
+    libexec.install "bin", "lib", "share"
     bin.install_symlink libexec/"bin/nvim"
   end
 
