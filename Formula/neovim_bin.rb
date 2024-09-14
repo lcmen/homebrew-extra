e6c4c60c70d988f3182459b90f90758772540c98
Lucas Mendelowski <lucas@mendelowski.com>
2024-09-14 17:12:35 +0200

    Update Neovim formula

--- Formula/neovim_bin.rb
+++ Formula/neovim_bin.rb
@@ -1,9 +1,9 @@
 class NeovimBin < Formula
   desc "Binary based formula for installing Neovim on MacOS."
   homepage "https://neovim.io"
-  url "https://github.com/neovim/neovim/releases/download/v0.10.0/nvim-macos-arm64.tar.gz"
-  version "0.10.0"
-  sha256 "e00452adbe1e90fb8c2d9bd41855b3f585bf6e2db31b9c35456d6253b0a152dd"
+  url "https://github.com/neovim/neovim/releases/download/v0.10.1/nvim-macos-arm64.tar.gz"
+  version "0.10.1"
+  sha256 "99cbc2aecb078caed426606e4496b736a975117cba028de79ca3ef19b2c42b16"
 
   conflicts_with "neovim", because: "neovim also ships neovim binary"
 
