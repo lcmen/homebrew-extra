227863dc7c2699f67747faab754472d7e7b4556f
lcmen <1379988+lcmen@users.noreply.github.com>
2026-03-29 09:08:40 +0000

    Update Formula/neovim_bin.rb to v0.11.7
    Co-Authored-By: GitHub Actions <noreply@github.com>

--- Formula/neovim_bin.rb
+++ Formula/neovim_bin.rb
@@ -1,16 +1,16 @@
 class NeovimBin < Formula
   desc "Binary based formula for installing Neovim on MacOS."
   homepage "https://neovim.io"
-  version "0.11.6"
+  version "0.11.7"
 
   on_arm do
-    url "https://github.com/neovim/neovim/releases/download/v0.11.6/nvim-macos-arm64.tar.gz"
-    sha256 "d5ee93b67630d5a87f3d336d0f7e8b2fe6973aa653d3cc93b82b820e6538c1dd"
+    url "https://github.com/neovim/neovim/releases/download/v0.11.7/nvim-macos-arm64.tar.gz"
+    sha256 "9c84686ce84bbab725ff3d88ca3ac1d6da5a72128b68d9dd8094e34d4049a126"
   end
 
   on_intel do
-    url "https://github.com/neovim/neovim/releases/download/v0.11.6/nvim-macos-x86_64.tar.gz"
-    sha256 "6e665bb0a711cf974050e0f7183dbb696b1e7425814eb87df7cb094a49054a06"
+    url "https://github.com/neovim/neovim/releases/download/v0.11.7/nvim-macos-x86_64.tar.gz"
+    sha256 "014649c0c75e188fd70d40f787ee7cb83e7c2926264cd4e72c300d0198922371"
   end
 
   conflicts_with "neovim", because: "neovim also ships neovim binary"
