4ba86cf365caf0cb21266b6fed6aafb62de5a564
lcmen <1379988+lcmen@users.noreply.github.com>
2026-05-13 15:38:05 +0000

    Update Formula/go-pty.rb to v0.3.0
    Co-Authored-By: GitHub Actions <noreply@github.com>

--- Formula/go-pty.rb
+++ Formula/go-pty.rb
@@ -1,16 +1,16 @@
 class GoPty < Formula
   desc "Terminal multiplexer for Go applications"
   homepage "https://github.com/lcmen/go-pty"
-  version "0.2.0"
+  version "0.3.0"
 
   on_arm do
-    url "https://github.com/lcmen/go-pty/releases/download/v0.2.0/go-pty-darwin-arm64"
-    sha256 "ffdd61f93998e907eea554a9660eb40c7f87a2ad8e43ee0842ad98da4e975e36"
+    url "https://github.com/lcmen/go-pty/releases/download/v0.3.0/go-pty-darwin-arm64"
+    sha256 "d5af9bf8e6369c9fcd77fe63e72c01a4306a92f619df5715af74a3e7721d761a"
   end
 
   on_intel do
-    url "https://github.com/lcmen/go-pty/releases/download/v0.2.0/go-pty-darwin-amd64"
-    sha256 "853f7fe71c001ef9a53f73e0406f29779f5d1f8eb0f2f3eee6f8f1f5d10f18d6"
+    url "https://github.com/lcmen/go-pty/releases/download/v0.3.0/go-pty-darwin-amd64"
+    sha256 "e347f57eeede9a72dcbeedd612263406dbd710929792af22cd43dafb8bfc0921"
   end
 
   def install
