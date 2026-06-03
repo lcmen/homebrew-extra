aff8bdd021e51eb274c8f8b14f11af897c5b596f
Lucas Mendelowski <lucas@mendelowski.com>
2026-06-03 21:41:29 +0200

    Fix audit errors

--- Formula/compartment.rb
+++ Formula/compartment.rb
@@ -3,26 +3,19 @@ class Compartment < Formula
   homepage "https://github.com/lcmen/compartment"
   version "0.2.0"
 
-  on_arm do
+  if Hardware::CPU.arm?
     url "https://github.com/lcmen/compartment/releases/download/v0.2.0/compartment-darwin-arm64"
     sha256 "12aa816480b86ab8c025523184e756417a8fe9b5d169759ad2a4a9b22e966393"
-  end
-
-  on_intel do
+  else
     url "https://github.com/lcmen/compartment/releases/download/v0.2.0/compartment-darwin-amd64"
     sha256 "fc6ec815ca8cb759e082a0cb12a92d9733795dac8539970bef73b29cf1771010"
   end
 
   def install
-    binary = if Hardware::CPU.arm?
-               "compartment-darwin-arm64"
-             else
-               "compartment-darwin-amd64"
-             end
-    bin.install binary => "compartment"
+    bin.install File.basename(stable.url) => "compartment"
   end
 
   test do
-    assert (bin/"compartment").exist?, "compartment binary does not exist"
+    assert_path_exists bin/"compartment"
   end
 end
--- Formula/go-pty.rb
+++ Formula/go-pty.rb
@@ -3,26 +3,19 @@ class GoPty < Formula
   homepage "https://github.com/lcmen/go-pty"
   version "0.3.0"
 
-  on_arm do
+  if Hardware::CPU.arm?
     url "https://github.com/lcmen/go-pty/releases/download/v0.3.0/go-pty-darwin-arm64"
     sha256 "d5af9bf8e6369c9fcd77fe63e72c01a4306a92f619df5715af74a3e7721d761a"
-  end
-
-  on_intel do
+  else
     url "https://github.com/lcmen/go-pty/releases/download/v0.3.0/go-pty-darwin-amd64"
     sha256 "e347f57eeede9a72dcbeedd612263406dbd710929792af22cd43dafb8bfc0921"
   end
 
   def install
-    binary = if Hardware::CPU.arm?
-               "go-pty-darwin-arm64"
-             else
-               "go-pty-darwin-amd64"
-             end
-    bin.install binary => "go-pty"
+    bin.install File.basename(stable.url) => "go-pty"
   end
 
   test do
-    assert (bin/"go-pty").exist?, "go-pty binary does not exist"
+    assert_path_exists bin/"go-pty"
   end
 end
